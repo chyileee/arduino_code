d09969abbdb5790f226618a7a8bc0df8cf9beed8
chyileee <chyileee@gmail.com>
2019-02-13 20:31:55 +0800

    UNO 使用Class, Struct, Union, Bit fields,測試OK

--- class_uno_data_member.ino
+++ class_uno_data_member.ino
@@ -3,7 +3,30 @@ class TestClass{
     int x = 1;
     int y = 2;
 };
- 
+struct modes
+{
+  unsigned int others : 1;  
+  unsigned int group  : 3;  
+  unsigned int user   : 5;  
+  unsigned int type   : 7;  
+}tbits;
+//modes tbits;
+
+union Number{
+  long int tlong;
+  int tint;
+  char tchar;
+  struct modes
+  {
+    unsigned int others : 1;  
+    unsigned int group  : 3;  
+    unsigned int user   : 5;  
+    unsigned int type   : 7;  
+  }tbits;
+  //modes tbits;
+};
+Number N1;
+//=============================
 void setup() {
   Serial.begin(115200);
  
@@ -19,7 +42,24 @@ void setup() {
  
   Serial.print("member x after assignment:");
   Serial.println(myObject.x);
- 
+  N1.tbits.others = 1;  //1-bit
+  N1.tbits.group = 6;   //3-bit
+  N1.tbits.user = 15;   //5-bit
+  N1.tbits.type = 10;   //7-bit
+  Serial.print("sizeof(tlong):");  Serial.println(sizeof(N1.tlong));
+  Serial.print("sizeof(tint):");  Serial.println(sizeof(N1.tint));
+  Serial.print("Number.tlong:");  Serial.println(N1.tlong,BIN);
+  Serial.print("Number.tint:");  Serial.println(N1.tint);
+  Serial.print("Number.tchar:");  Serial.println(N1.tchar,HEX);
+  Serial.print("Number.tbits:");  Serial.println(N1.tbits.type );
+  N1.tlong=0xe123457f;
+  Serial.print("Number.tlong:");  Serial.println(N1.tlong,BIN);
+  Serial.print("Number.tint:");  Serial.println(N1.tint);
+  Serial.print("Number.tchar:");  Serial.println(N1.tchar,HEX);
+  Serial.print("N1.tbits.others:");  Serial.println(N1.tbits.others );
+  Serial.print("N1.tbits.group:");  Serial.println(N1.tbits.group );
+  Serial.print("N1.tbits.user:");  Serial.println(N1.tbits.user );
+  Serial.print("N1.tbits.type:");  Serial.println(N1.tbits.type );
 }
  
 void loop() {}
